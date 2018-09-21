4ace6ede7961bed310b205d7789c43183a8b504b
Spentplaying <jordan820926@gmail.com>
2018-09-21 17:31:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/spentplaying/editview/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.spentplaying.editview;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.btn_edit);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Edit.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
