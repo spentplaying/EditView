3f630868e35fa6839a838c4891f380ff4d807da8
Spentplaying <jordan820926@gmail.com>
2018-09-22 00:36:00 +0800

    Fix the bug when attempt to intent a new activity.

--- app/src/main/java/com/example/spentplaying/editview/Edit.java
+++ app/src/main/java/com/example/spentplaying/editview/Edit.java
@@ -1,15 +1,60 @@
 package com.example.spentplaying.editview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Edit extends AppCompatActivity {
+    private String f_name = "";
+    private String l_name = "";
+    private String e_mail = "";
+
+    void setview(){
+        EditText f_n = (EditText) findViewById(R.id.f_name);
+        f_n.setText(f_name);
+        EditText l_n = (EditText) findViewById(R.id.l_name);
+        l_n.setText(l_name);
+        EditText em = (EditText) findViewById(R.id.email);
+        em.setText(e_mail);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit);
+        f_name = getIntent().getStringExtra("f_name");
+        l_name = getIntent().getStringExtra("l_name");
+        e_mail = getIntent().getStringExtra("email");
+        setview();
+        Button btn_cancel = (Button) findViewById(R.id.cancel);
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultIntent = new Intent();
+                setResult(RESULT_CANCELED,resultIntent);
+                finish();
+            }
+        });
+        Button btn_save = (Button) findViewById(R.id.save);
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultIntent = new Intent();
+                EditText tx_l = (EditText) findViewById(R.id.l_name);
+                EditText tx_f = (EditText) findViewById(R.id.f_name);
+                EditText tx_e = (EditText) findViewById(R.id.email);
+                f_name = tx_f.getText().toString();
+                l_name = tx_l.getText().toString();
+                e_mail = tx_e.getText().toString();
+                resultIntent.putExtra("f_name",f_name);
+                resultIntent.putExtra("l_name",l_name);
+                resultIntent.putExtra("email",e_mail);
+                setResult(RESULT_OK,resultIntent);
+                finish();
+            }
+        });
     }
 }
 
--- app/src/main/java/com/example/spentplaying/editview/MainActivity.java
+++ app/src/main/java/com/example/spentplaying/editview/MainActivity.java
@@ -3,22 +3,48 @@ package com.example.spentplaying.editview;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String f_name = "Keng";
+    private String l_name = "Chang";
+    private String e_mail = "jordan820926@gmail.com";
+    static final int Result = 0 ;
+    void setview(){
+        EditText f_n = (EditText) findViewById(R.id.f_name);
+        f_n.setText(f_name);
+        EditText l_n = (EditText) findViewById(R.id.l_name);
+        l_n.setText(l_name);
+        EditText em = (EditText) findViewById(R.id.email);
+        em.setText(e_mail);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setview();
         Button btn = (Button) findViewById(R.id.btn_edit);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Edit.class);
-                startActivity(intent);
+                intent.putExtra("f_name",f_name);
+                intent.putExtra("l_name",l_name);
+                intent.putExtra("email",e_mail);
+                startActivityForResult(intent,0);
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode == RESULT_OK){
+            f_name = data.getStringExtra("f_name");
+            l_name = data.getStringExtra("l_name");
+            e_mail = data.getStringExtra("email");
+            setview();
+        }
     }
 }
